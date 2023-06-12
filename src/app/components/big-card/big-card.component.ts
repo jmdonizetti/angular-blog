78fb3776d160cfc77ba945c7d5adb8b716d2600f
Joao <j_donizetti@outlook.com>
2023-06-12 18:28:04 -0300

    Blog finalizado

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -14,6 +14,8 @@ export class BigCardComponent implements OnInit {
   cardTitle:string= ""
   @Input()
   cardDescription:string = ""
+  @Input()
+  id:string = "0"
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Novo jogo do Homem de ferro",
+    "description": "Com a parceria da Marvel e Motive o novo jogo do Homem de ferro esta sendo desenvolvido",
+    "photoCover": "https://media.contentapi.ea.com/content/dam/news/www-ea/images/2022/09/ea-motive-new-title-teaser-16x9-featured.jpg.adapt.crop16x9.575p.jpg"
+  },
+  {
+     "id":"2",
+    "title": "Filme do Doutor Estranho no Multiverso da loucura",
+    "description": "Filme do Doutor Estranho no Multiverso da loucura",
+    "photoCover": "https://musicart.xboxlive.com/7/ccb25f00-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"
+  },
+  {
+    "id":"3",
+   "title": "Novo filme do Homem Aranha atraves aranha-verso",
+   "description": "Novo filme do homem aranha (Aranha-verso) conta com vários tipos de homem-aranhas que você pode imaginar",
+   "photoCover": "https://www.rbsdirect.com.br/imagesrc/35798348.jpg?version=1575255600"
+ },
+ {
+  "id":"4",
+ "title": "Filme do Pantera Negra 2 é um tributo ao ator Chadwick Boseman",
+ "description": "Filme do Pantera Negra dois é um tributo ao ator Chadwick Boseman",
+ "photoCover": "https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2022/11/09/a6fe0e439a9ff4e304e0b6ff30992141_xl-sl14m8j1y9gl.jpg"
+}
+
+]
